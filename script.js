0960c0c5fc65c1aa7704b5ee173bc8503347a5cd
JillWesterfelhaus <jillwesthaus@gmail.com>
2020-02-27 14:57:07 -0600

    Updated index

--- script.js
+++ script.js
@@ -1,52 +1,52 @@
 var myQuestion = {
-	question: 'What is 1+1?',
-  answers: [
-  	'5',
-    '2',
-    '0',
-    '1'
-  ],
-  rightAnswer: 1
+    question: 'What is 1+1?',
+    answers: [
+        '5',
+        '2',
+        '0',
+        '1'
+    ],
+    rightAnswer: 1
 }
 
 var questions = [
-	{
-    question: 'What is 1+1?',
-    answers: [
-      '5',
-      '2',
-      '0',
-      '1'
-    ],
-    rightAnswer: 1
-  },
-  {
-    question: 'What is 2+2?',
-    answers: [
-      '4',
-      '2',
-      '0',
-      '1'
-    ],
-    rightAnswer: 0
-  }
+    {
+        question: 'What is 3+3?',
+        answers: [
+            '5',
+            '2',
+            '6',
+            '1'
+        ],
+        rightAnswer: 2
+    },
+    {
+        question: 'What is 2+2?',
+        answers: [
+            '4',
+            '2',
+            '0',
+            '1'
+        ],
+        rightAnswer: 0
+    }
 ]
 
 function generateQuestion(question) {
-	var el = document.getElementById('question')
-  el.innerHTML = ''
-  var questionDiv = document.createElement('div')
-  questionDiv.innerHTML = question.question
-  el.appendChild(questionDiv)
-  
-  for(var i = 0; i < question.answers.length; i++) {
-  	var answerDiv = document.createElement('div')
-    answerDiv.innerHTML = question.answers[i]
-    if(question.rightAnswer == i) {
-    	answerDiv.setAttribute('data-correct', 'true')
+    var el = document.getElementById('question')
+    el.innerHTML = ''
+    var questionDiv = document.createElement('div')
+    questionDiv.innerHTML = question.question
+    el.appendChild(questionDiv)
+
+    for (var i = 0; i < question.answers.length; i++) {
+        var answerDiv = document.createElement('div')
+        answerDiv.innerHTML = question.answers[i]
+        if (question.rightAnswer == i) {
+            answerDiv.setAttribute('data-correct', 'true')
+        }
+        el.appendChild(answerDiv)
     }
-    el.appendChild(answerDiv)
-  }
 }
 
 generateQuestion(questions[1])
